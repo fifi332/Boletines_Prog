10d4772aea2213d02f90d900036bf6b5ae155bd9
andre <aperazanascimento@danielcastelao.org>
2025-10-14 14:05:34 +0200

    ejercicio contraseña

---
+++ ejer_contra.py
@@ -0,0 +1,30 @@
+import random
+
+def generar_contraseña(longitud=8):
+    if longitud < 6:
+        longitud = 6
+    if longitud > 12:
+        longitud = 12
+
+    mayus = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+    minus = 'abcdefghijklmñopqrstuvwxyz'
+    numeros = '0123456789'
+    simbolos = '°|¬!#$%&/()=?¡¿+*~[]{};:,.<>'
+    todos = mayus + minus + numeros + simbolos
+
+    contraseña = ''
+    for _ in range(longitud):
+        con = random.randint(0, len(todos))
+        contraseña += todos[con]
+    return contraseña
+
+while True:
+    n = int(input("Ingrese la longitud de la contraseña (6-12): "))
+    if n >= 6 and n <= 12:
+        print(generar_contraseña(n))
+    elif n == 0:
+        break
+    else:
+        print("eres medio marico verdad?")
+
+print(generar_contraseña(12))
